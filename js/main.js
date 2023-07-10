a36f31d5953879f097819b620acc98b72088fd9f
gitseodong <seodong56@nate.com>
2023-07-11 01:23:01 +0900

    7/11 01:22

--- js/main.js
+++ js/main.js
@@ -17,6 +17,32 @@ inputEl.addEventListener('blur', () => {
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll', () => {
-  console.log(window.scrollY);
+window.addEventListener(
+  'scroll',
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      //배지 숨기기
+      //gsap.to(요소, 지속시간(초), 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: 'none',
+      });
+    } else {
+      //배지 보이기
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: 'block',
+      });
+    }
+  }, 300)
+);
+// _.throttle(함수, 시간(ms))
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
 });
