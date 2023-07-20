ccf2f7530f78e601ca7991251bdc2eb95b354a39
gitseodong <seodong56@nate.com>
2023-07-21 01:34:29 +0900

    2023-07-21 오전1시34분스타벅스 예제 완성

--- js/main.js
+++ js/main.js
@@ -16,6 +16,7 @@ inputEl.addEventListener('blur', () => {
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener(
   'scroll',
@@ -27,20 +28,35 @@ window.addEventListener(
         opacity: 0,
         display: 'none',
       });
+      //버튼 보이기!
+      gsap.to(toTopEl, 0.2, {
+        x: 0,
+      });
     } else {
       //배지 보이기
       gsap.to(badgeEl, 0.6, {
         opacity: 1,
         display: 'block',
       });
+      //버튼 숨기기!
+      gsap.to(toTopEl, 0.2, {
+        x: 100,
+      });
     }
   }, 300)
 );
 // _.throttle(함수, 시간(ms))
 
+toTopEl.addEventListener('click', () => {
+  gsap.to(window, 0.7, {
+    scrollTo: 0,
+  });
+});
+
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 
 fadeEls.forEach((fadeEl, index) => {
+  //gsap.to(요소, 시간, 옵션);
   gsap.to(fadeEl, 1, {
     delay: (index + 1) * 0.7,
     opacity: 1,
@@ -59,9 +75,9 @@ new Swiper('.promotion .swiper-container', {
   spaceBetween: 10, // 슬라이드 사이 여백
   centeredSlides: true, // 1번 슬라이드가 가운데 보이기
   loop: true,
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  autoplay: {
+    delay: 5000,
+  },
   pagination: {
     el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
     clickable: true, // 사용자의 페이지 번호 요소 제어 가능 여부
@@ -69,6 +85,17 @@ new Swiper('.promotion .swiper-container', {
   navigation: {
     prevEl: '.promotion .swiper-prev',
     nextEl: '.promotion .swiper-next',
+  },
+});
+
+new Swiper('.awards .swiper-container', {
+  autoplay: true,
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    prevEl: '.awards .swiper-prev',
+    nextEl: '.awards .swiper-next',
   },
 });
 
@@ -85,3 +112,44 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide');
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  //gsap.to(요소, 시간, 옵션);
+  gsap.to(
+    selector, // 선택자
+    random(1.5, 2.5), // 애니메이션 동작 시간
+    {
+      // 옵션
+      y: size,
+      repeat: -1,
+      yoyo: true,
+      ease: Power1.easeInOut,
+      delay: random(0, delay),
+    }
+  );
+}
+
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+    triggerHook: 0.8, //화면에 보여졌다고 판단할 기준 위치를 지정.
+  })
+    .setClassToggle(spyEl, 'show') //화면에 보여지면 show라는 클래스를 추가함.
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+
+thisYear.textContent = new Date().getFullYear(); //2023
